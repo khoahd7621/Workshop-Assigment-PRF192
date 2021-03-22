2f50e5aa97a9107e3ce12662b61308597a546b17
Hoang Dang Khoa <77594830+khoahd7621@users.noreply.github.com>
2021-03-22 21:36:40 +0700

    Update Workshop2-Program1.c

--- Workshop2-Program1.c
+++ Workshop2-Program1.c
@@ -5,27 +5,27 @@
 
 int main()
 {
-    int a, b;
-    char c;
-    printf("Enter simple expression: ");
-	scanf("%d %c %d", &a, &c, &b);
-    	switch (c)
+	int a, b;
+	char c;
+	printf("Enter simple expression: ");
+	scanf("%d%c%d", &a, &c, &b);
+    		switch (c)
 		{
 			case '+':
-                printf("%d + %d = %d\n", a, b, a + b);
-                break;
-            case '-':
-                printf("%d - %d = %d\n", a, b, a - b);
-                break;
-            case '*':
-                printf("%d * %d = %d\n", a, b, a * b);
-                break;
-            case '/':
-                if ( b == 0) {
-                    printf("Cannot devide 0\n"); }
-                else {
-                    printf("%d / %d = %d\n", a, b, a / b); }
-                break;
-    	}
-    return 0;
+				printf("%d + %d = %d\n", a, b, a + b);
+                		break;
+            		case '-':
+                		printf("%d - %d = %d\n", a, b, a - b);
+                		break;
+            		case '*':
+				printf("%d * %d = %d\n", a, b, a * b);
+                		break;
+            		case '/':
+                		if ( b == 0)
+                    			printf("Cannot devide 0\n");
+                		else
+                    			printf("%d / %d = %d\n", a, b, a / b);
+                		break;
+		}
+	return 0;
 }
