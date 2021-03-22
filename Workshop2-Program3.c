e880bf0e1a2d923af47663aa39e1e4830f992cb1
Hoang Dang Khoa <77594830+khoahd7621@users.noreply.github.com>
2021-03-22 21:41:51 +0700

    Update Workshop2-Program3.c

--- Workshop2-Program3.c
+++ Workshop2-Program3.c
@@ -2,18 +2,15 @@
 
 #include <stdio.h>
 
-int main() {
-    
-    int a, sum = 0;
-    
+int main()
+{
+    int a, sum = 0; 
     printf("Enter integers number: ");
-    
-    while (a != 0) {
+    while (a != 0)
+    {
     	scanf("%d", &a);
         sum = sum + a;
     }
-    
     printf("Sum of integers entered: %d", sum);
-    
     return 0;
 }
