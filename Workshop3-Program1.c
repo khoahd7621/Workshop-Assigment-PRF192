1e63e645f776b94545b0fb65e1f3dcbc103ed64a
Hoang Dang Khoa <77594830+khoahd7621@users.noreply.github.com>
2021-03-22 21:56:13 +0700

    Update Workshop3-Program1.c

--- Workshop3-Program1.c
+++ Workshop3-Program1.c
@@ -1,34 +1,33 @@
-//Accept a positive integer n, n>=2
-//then print out primes between 2 and n
+// Accept a positive integer n, (n >= 2) then print out primes between 2 and n
 
 #include <stdio.h>
 #include <math.h>
 
-int prime(int n) {
-	int m = sqrt(n);
+int prime(int n)
+{
 	int s = 1;
-    int i;
-    if (n < 2) s = 0;
-    for (i = 2; i <= m; i++)
-        if (n % i == 0) {
+	if (n < 2)
 		s = 0;
-		}
-    return s; 
+	int i;
+	for (i = 2; i <= sqrt(n); i++)
+        	if (n % i == 0)
+			s = 0;
+	return s; 
 }
 
 int main ()
 {
 	int n, i, s;
-    printf("Enter positive interger: ");
-    scanf("%d", &n);
+	printf("Enter positive interger: ");
+	scanf("%d", &n);
 	printf("Prime numbers between 2 and %d are: ", n);
-	if ( n <= 3)
+	if (n <= 3)
 		printf("Error! Condition is not satisfied or between two numbers do not have integer.");
-	for ( i = 3; i < n; i++)
+	for (i = 3; i < n; i++)
 	{
-	    s = prime(i);
-        if (s == 1)
-        printf("%d ", i);
-    }
-    return 0;
+		s = prime(i);
+        	if (s == 1)
+        		printf("%d ", i);
+	}
+	return 0;
 }
